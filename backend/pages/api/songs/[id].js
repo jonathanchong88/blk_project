000e130b05f9a69f906a29bd23d0ffc549828b34
cjze <140057147+cjze@users.noreply.github.com>
2026-02-16 10:08:37 +0800

    solve options issue

--- backend/pages/api/songs/[id].js
+++ backend/pages/api/songs/[id].js
@@ -20,6 +20,10 @@ export default async function handler(req, res) {
         await runMiddleware(req, res, corsMiddleware);
     }
 
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     const { id } = req.query;
 
     if (req.method === 'GET') {
